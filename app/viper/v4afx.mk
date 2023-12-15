80e391279e7f6bdb46265a395057ccb32344a504
Nishant Kumar <www.rajsonu13@gmail.com>
2023-12-15 22:24:04 +0530

    RMX2020: Import Viper4Android drivers
    * Thanks to Viper4Android team for drivers
    
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>
    Signed-off-by: Kousick Dey <kousickdey3@gmail.com>

---
+++ app/viper/v4afx.mk
@@ -0,0 +1,8 @@
+DEVICE_PATH := device/realme/RMX2020
+
+# ViPER4AndroidFX
+PRODUCT_PACKAGES += \
+    ViPER4AndroidFX
+
+# Time to relax
+RELAX_USES_LIBRARY_CHECK := true
---
+++ vendorsetup.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/Lineage-RMX2020/android_vendor_prebuilts.git vendor/prebuilts
